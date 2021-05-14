992f9dc730e55fe53ef63c4578e7d61e4720f63a
unknown <you@example.c>
2021-05-14 22:50:46 +0300

    12

---
+++ 12.05.2021/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12._05._2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 9
+            Console.WriteLine("Введите число a");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите число b");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите число y1");
+            double y1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите число y2");
+            double y2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите число x1");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите число x2");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите число o1");
+            double o1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите число o2");
+            double o2 = Convert.ToDouble(Console.ReadLine());
+            double y = a * x1 + b;
+            double x = y1 - a * x1;
+            double y = y1 - y2 / x1 - x2;
+            Console.WriteLine("y");
+            Console.ReadKey();   
+
+
+        }
+    }
+}
---
+++ 19/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //19
+            Console.WriteLine("Введите число c1");
+            double c1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите число n1");
+            double  n1 = Convert.ToDouble(Console.ReadLine());
+            double  c = c1 + n1;
+            double x = c1 * 100 / c1 + n1;
+            double y = c1 * (100 * x) / 100;
+            Console.WriteLine("сумма в %");
+            Console.ReadKey();
+        }
+    }
+}
